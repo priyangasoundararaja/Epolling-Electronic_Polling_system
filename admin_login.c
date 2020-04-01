878e081206027212cf6324eb5705429a8f21b2d5
Aswanth-Ramamoorthy <aswanthramamoorthy@gmail.com>
2020-04-01 17:32:14 -0400

    Admin Authentication

---
+++ admin_login.c
@@ -0,0 +1,35 @@
+#include<conio.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#pragma warning(disable:4996)
+
+void admin_login()
+{
+    char adminID[15];
+    char adminpass[12];
+    int i;
+    printf("Enter your Admin-ID:\n");
+    scanf("%s", &adminID);
+    printf("Enter your password:\n");
+    for (i = 0; i < 8; i++)
+    {
+        adminpass[i] = getch();
+        printf("*");
+    }
+    adminpass[i] = '\0';
+    if (strcmp(adminID, "Admin") == 0)
+    {
+        if (strcmp(adminpass, "white!23") == 0)
+        {
+            printf("\nWelcome.Login Success!");
+        }
+        else {
+            printf("\nwrong password");
+        }
+    }
+    else {
+        printf("\nUser doesn't exist");
+    }
+
+}
